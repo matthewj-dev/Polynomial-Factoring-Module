a79a37d7e2ca4a5bb1d13e84cf255519e478d31d
pwhite97 <32716708+pwhite97@users.noreply.github.com>
2017-11-08 14:35:21 -0600

    Update synthdiv.py

--- synthdiv.py
+++ synthdiv.py
@@ -3,18 +3,22 @@ def synthetic_div(polynomial, factor_list):
     count = 0
     tempval = 0
     result = []
-    if count < len(factor_list):
+    for j in range(count, len(factor_list)):
         for i in range(0, len(polynomial)):
-            tempval += polynomial[i] 
+            print(tempval)
+            tempval = tempval + polynomial[i] 
+            print(tempval)
             tempval = tempval * factor_list[count]
-        
+            print(tempval)
+
         if tempval == 0:
              result.append(factor_list[count])
+        tempval = 0    
         count += 1
     return result
 
 if __name__ == '__main__':
         print ("POLYNOMIAL SYNTHETIC DIVISION")
-        N = [1, 3, 5]
-        D = [1, -3, -2, 2, 3]
+        N = [4, -19, 12]
+        D = [0.75, 1.5, 1.0, 3, -0.75, 0.5, -0.5, 0.25, -0.25, 6, 2, 4, 12, -12, -2, -6, -4, -3, -1, -1.5]
         print (synthetic_div(N, D))
